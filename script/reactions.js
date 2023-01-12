fdbf571cd307c1011b989cae3e7955470ffb715d
unknown <348336843@TDSB.ON.CA>
2023-01-12 10:15:45 -0500

    added background and buttons to chemical reactions

--- script/reactions.js
+++ script/reactions.js
@@ -1,6 +1,24 @@
 var canvas = document.getElementById("reaction-simulation");
 if (canvas.getContext) var ctx = canvas.getContext("2d"); else alert("Canvas element is not available");
 
+var width = canvas.width;
+var height = canvas.height;
+
+var synCol = "white";
+var decCol = "white";
+var sDisCol = "white";
+var dDisCol = "white";
+var cComCol = "white";
+var iComCol = "white";
+
+
+var frameCnt = 0;
+
+const intervalID = setInterval(draw, 10);         
+
+var bg1 = new Image();
+bg1.src = "assets/chemBG.png";
+
 //returns the mouse position
 function getMousePos(canvas, event) {
     var rect = canvas.getBoundingClientRect();
@@ -11,77 +29,142 @@ function getMousePos(canvas, event) {
 }
 
 //Binding the click event on the canvas
-canvas.addEventListener('click', function(evt) {
+canvas.addEventListener('mousemove', function(evt) {
     var mousePos = getMousePos(canvas, evt);
 
-    //do smth if the mouse is clicked
+    if(isInside(mousePos, synthesis)){
+        synCol = "yellow";
+    }else{
+        synCol = "white";
+    }
+
+    if(isInside(mousePos, decomposition)){
+        decCol = "yellow";
+    }else{
+        decCol = "white";
+    }
+
+    if(isInside(mousePos, sDisplace)){
+        sDisCol = "yellow";
+    }else{
+        sDisCol = "white";
+    }
+
+    if(isInside(mousePos, dDisplace)){
+        dDisCol = "yellow";
+    }else{
+        dDisCol = "white";
+    }
+
+    if(isInside(mousePos, cCombustion)){
+        cComCol = "yellow";
+    }else{
+        cComCol = "white";
+    }
+
+    if(isInside(mousePos, iCombustion)){
+        iComCol = "yellow";
+    }else{
+        iComCol = "white";
+    }
 
 
 
 }, false);
 
+canvas.addEventListener('mousedown', function(evt) {
+    var mousePos = getMousePos(canvas, evt);
 
-/*
+
+}, false);
+
+
 function isInside(pos, rect){
     return pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.height && pos.y > rect.y
-}*/
+}
 
 
 //6 objects to hold the 6 different animations
 var synthesis = {
-    x:150,
-    y:200,
-    width:200,
-    height:100
+    x:112,
+    y:235,
+    width:250,
+    height:125
 };
 
 var decomposition = {
-    x:500,
-    y:200,
-    width:200,
-    height:100
+    x:474,
+    y:235,
+    width:250,
+    height:125
 };
 
 var sDisplace = {
-    x:850,
-    y:200,
-    width:200,
-    height:100
+    x:836,
+    y:235,
+    width:250,
+    height:125
 };
 
 var dDisplace = {
-    x:100,
-    y:400,
-    width:200,
-    height:100
+    x:112,
+    y:448,
+    width:250,
+    height:125
 };
 
 var cCombustion ={
-    x:450,
-    y:400,
-    width:200,
-    height:100
+    x:474,
+    y:448,
+    width:250,
+    height:125
 };
 
 var iCombustion = {
-    x:800,
-    y:400,
-    width:200,
-    height:100
+    x:836,
+    y:448,
+    width:250,
+    height:125
 };
 
 
 
 function draw(){
-    ctx.fillStyle = "grey";
-    ctx.roundRect(synthesis.x,synthesis.y,synthesis.width,synthesis.height,10);
-    ctx.roundRect(decomposition.x,decomposition.y,decomposition.width,decomposition.height,10);
-    ctx.roundRect(sDisplace.x,sDisplace.y,sDisplace.width,sDisplace.height,10);
-    ctx.roundRect(dDisplace.x,dDisplace.y,dDisplace.width,dDisplace.height,10);
-    ctx.roundRect(cCombustion.x,cCombustion.y,cCombustion.width,cCombustion.height,10);
-    ctx.roundRect(iCombustion.x,iCombustion.y,iCombustion.width,iCombustion.height,10);
+    if(frameCnt == 0){
+        ctx.drawImage(bg1, 0, 0);
 
-    ctx.fill();
+        ctx.font = "80px Comic Sans MS";
+        ctx.fillStyle = "red";
+        ctx.textAlign = "center";
+        ctx.fillText("Chemical Reactions Simulator", canvas.width/2, 125);
+
+        //drawing the buttons to move to different animation
+        ctx.fillStyle = synCol;
+        ctx.roundRect(synthesis.x,synthesis.y,synthesis.width,synthesis.height,10);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.fillStyle = decCol;
+        ctx.roundRect(decomposition.x,decomposition.y,decomposition.width,decomposition.height,10);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.fillStyle = sDisCol;
+        ctx.roundRect(sDisplace.x,sDisplace.y,sDisplace.width,sDisplace.height,10);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.fillStyle = dDisCol;
+        ctx.roundRect(dDisplace.x,dDisplace.y,dDisplace.width,dDisplace.height,10);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.fillStyle = cComCol;
+        ctx.roundRect(cCombustion.x,cCombustion.y,cCombustion.width,cCombustion.height,10);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.fillStyle = iComCol;
+        ctx.roundRect(iCombustion.x,iCombustion.y,iCombustion.width,iCombustion.height,10);
+        ctx.fill();
+        ctx.beginPath();
+    }
+
 
 }
 
