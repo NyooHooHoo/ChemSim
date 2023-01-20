38a17572c15cd398154bfc2dae5e323ee78d1cf9
Aidan Wang <aidan@MacBook-2744.local>
2023-01-20 10:19:10 -0500

    buttons haha

--- script/builder.js
+++ script/builder.js
@@ -1,45 +1,40 @@
-var canvas = document.getElementById("builder-canvas");
-var ctx = canvas.getContext("2d");
-var width = canvas.width;
-var height = canvas.height;
-let intervalID;
-var mousePos;
+let canvas = document.getElementById("builder-canvas");
+let ctx = canvas.getContext("2d");
+let width = canvas.width;
+let height = canvas.height;
+let mousePos;
+
 
 let borders = [
 	{x: 0, y: 0, width: 1000, height: 60}, 
 	{x: 0, y: 540, width: 1000, height: 60}, 
 	{x: 0, y: 0, width: 20, height: 600}, 
 	{x: 980, y: 0, width: 20, height: 600}, 
-];
+	];
 
-let buttons = {};
-var names = ["clear", "erase", "move", "single", "double", "triple", "H", "C", "O", "diagram"];
-var startX = 20;
-for (var i = 0; i < names.length; i++) {
-	buttons[names[i]] = {x: startX, y: 10, width: 40, height: 40};
+let atoms = [];
+
+let buttons = [];
+let names = ["clear", "erase", "move", "single", "double", "triple", "B", "C", "H", "N", "O", "P", "S", "F", "Cl", "Br", "I", "diagram"];
+let startX = 50;
+for (let i = 0; i < names.length; i++) {
+	buttons.push({x: startX, y: 10, width: 40, height: 40, name: names[i]});
 	startX += 50;
 }
-
-
-// canvas.addEventListener("mousedown", function (evt) {
-//     mousePos = getMousePos(canvas, evt);
-//     if (isInside(mousePos, space))
-//     	intervalID = setInterval(draw, 10);
-// });
 
 canvas.addEventListener("mousemove", function (evt) {
     mousePos = getMousePos(canvas, evt);
 });
 
-// canvas.addEventListener("mouseup", function () {
-// 	clearInterval(intervalID);
-// 	background();
-// });
-
+canvas.addEventListener("click", function (evt) {
+    atoms.push({x: mousePos.x, y: mousePos.y});
+});
 
 function animate() {
 	window.requestAnimationFrame(animate);
 	background();
+	drawBonds();
+	drawAtoms();
 	menus();
 }
 
@@ -52,14 +47,34 @@ function background() {
 function menus() {
 	ctx.beginPath();
 	ctx.fillStyle = "#31f791";
-	for (var i = 0; i < borders.length; i++) {
+	for (let i = 0; i < borders.length; i++) {
 		b = borders[i];
 		ctx.fillRect(b.x, b.y, b.width, b.height);
 	}
-	ctx.fillStyle = "black";
-	for (var i = 0; i < buttons.length; i++) {
+	ctx.textBaseline = "middle";
+	ctx.font = "24px sans-serif";
+    ctx.textAlign = "center";
+	for (let i = 0; i < buttons.length; i++) {
 		b = buttons[i];
+		ctx.fillStyle = "grey";
 		ctx.fillRect(b.x, b.y, b.width, b.height);
+		ctx.fillStyle = "black";
+		ctx.fillText(b.name[0], b.x + b.width/2, b.y + b.height/2);
+	}
+}
+
+function drawBonds() {
+
+}
+
+function drawAtoms() {
+	ctx.fillStyle = "red";
+	let a;
+	for (let i = 0; i < atoms.length; i++) {
+		ctx.beginPath();
+		a = atoms[i];
+		ctx.arc(a.x, a.y, 10, 0, 2 * Math.PI);
+		ctx.fill();
 	}
 }
 
@@ -68,7 +83,7 @@ function isInside(pos, rect){
 }
 
 function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
+    let rect = canvas.getBoundingClientRect();
     return {
         x: evt.clientX - rect.left,
         y: evt.clientY - rect.top
